9176ee0a0247a66adbab1e2212c5a4942d82cae6
Leonardo Molinari <207478123+leomolinari14@users.noreply.github.com>
2026-09-08 15:34:54 -0300

    feat: adiciona edicao de pacientes

--- Controllers/PacientesController.cs
+++ Controllers/PacientesController.cs
@@ -39,4 +39,50 @@ public class PacientesController : Controller
 
         return RedirectToAction(nameof(Index));
     }
+
+    public IActionResult Edit(int id)
+    {
+        var paciente = _context.Pacientes.Find(id);
+
+        if (paciente is null)
+        {
+            return NotFound();
+        }
+
+        return View(paciente);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(
+        int id,
+        [Bind("Id,Nome,Cpf,Telefone,Endereco,DataNascimento")] Paciente paciente)
+    {
+        if (id != paciente.Id)
+        {
+            return NotFound();
+        }
+
+        if (!ModelState.IsValid)
+        {
+            return View(paciente);
+        }
+
+        var pacienteExistente = _context.Pacientes.Find(id);
+
+        if (pacienteExistente is null)
+        {
+            return NotFound();
+        }
+
+        pacienteExistente.Nome = paciente.Nome;
+        pacienteExistente.Cpf = paciente.Cpf;
+        pacienteExistente.Telefone = paciente.Telefone;
+        pacienteExistente.Endereco = paciente.Endereco;
+        pacienteExistente.DataNascimento = paciente.DataNascimento;
+
+        _context.SaveChanges();
+
+        return RedirectToAction(nameof(Index));
+    }
 }
