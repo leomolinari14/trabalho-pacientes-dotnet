f906d337d17825c545c3343ccf768996782ff34f
Leonardo Molinari <207478123+leomolinari14@users.noreply.github.com>
2026-09-08 14:50:21 -0300

    feat: cria migration inicial de pacientes

---
+++ Migrations/20260908174444_CriacaoInicial.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ProjetoAgendamento.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriacaoInicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pacientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Cpf = table.Column<string>(type: "character varying(14)", maxLength: 14, nullable: false),
+                    Telefone = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: false),
+                    Endereco = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    DataNascimento = table.Column<DateTime>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pacientes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pacientes");
+        }
+    }
+}
